9958a96c58bd69dd8729b8e9e8f6f212a5785419
Hagar Dauch <mm9gtan@gmail.com>
2017-01-03 11:27:10 +0000

    Cleaned up unneunnecessary contact for routes and adjusted to new contact me page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
     root to: "pages#home"
     get "about",to: "pages#about"
-    resources :contacts
-    get 'contact-me', to: 'contacts#new'  
+    resources :contacts, only: :create
+    get 'contact-me', to: 'contacts#new', as: "new_contact" #changes path of new_contact to path contact-me, allowing application view file to need no changing 
 end
